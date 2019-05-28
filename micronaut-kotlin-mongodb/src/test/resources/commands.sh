53e98d231211e8d7ce7fb0ac4c17facde2e79b62
csh0711 <christian.schwoerer@novatec-gmbh.de>
2019-05-28 20:42:45 +0200

    Fix shell scripts

--- micronaut-kotlin-mongodb/src/test/resources/commands.sh
+++ micronaut-kotlin-mongodb/src/test/resources/commands.sh
@@ -2,9 +2,8 @@
 mn create-app info.novatec.footballmanager --features=mongo-reactive --lang=kotlin
 
 ## CURLs
-curl http://localhost:8080/footballmanager/ | json_pp
+curl http://localhost:8080/footballers/ | json_pp
 
-curl -X POST -H "Content-Type: application/json" -d '{"firstName" : "Timo","lastName" : "Baumgartl","position" : "Defense"}' http://localhost:8080/footballmanager/
+curl -X POST -H "Content-Type: application/json" -d '{"firstName" : "Leon","lastName" : "Goretzka","position" : "Midfield"}' http://localhost:8080/footballers/
 
-curl -X DELETE  http://localhost:8080/footballmanager/5c66d85c4d34bde723de0641
-
+curl -X DELETE  http://localhost:8080/footballers/[id]
